395402fbbf042cdbac475a2ee29127ca947fb29a
Dmitriy Fomin <79067773397@yandex.ru>
2020-12-28 22:46:15 +0300

    remove coding

--- chat-server/network/src/rufomin/network/SocketThread.java
+++ chat-server/network/src/rufomin/network/SocketThread.java
@@ -6,13 +6,10 @@ import java.net.SocketException;
 import java.util.Base64;
 
 public class SocketThread extends Thread {
-    private final static byte CODE_NUMBER = 77;
     private final SocketThreadListener listener;
     private final Socket socket;
     private DataOutputStream out;
     DataInputStream in;
-    private final static Base64.Encoder ENCODER = Base64.getEncoder();
-    private final static Base64.Decoder DECODER = Base64.getDecoder();
 
     public SocketThread(SocketThreadListener listener, String name, Socket socket) {
         super(name);
@@ -31,7 +28,6 @@ public class SocketThread extends Thread {
             listener.onSocketReady(this, socket);
             while (!isInterrupted()) {
                 String msg = in.readUTF();
-                msg = decoding(msg);
                 listener.onReceiveString(this, socket, msg);
             }
         } catch (EOFException | SocketException e) {
@@ -47,9 +43,10 @@ public class SocketThread extends Thread {
         }
     }
 
+
+
     public synchronized boolean sendMessage(String msg) {
         try {
-            msg = encoding(msg);
             out.writeUTF(msg);
             out.flush();
             return true;
@@ -60,23 +57,6 @@ public class SocketThread extends Thread {
         }
     }
 
-    private String encoding(String msg) {
-        byte[] code = ENCODER.encode(msg.getBytes());
-        codingByteArray(code);
-        return new String(code);
-    }
-
-    private String decoding(String msg) {
-        byte[] code=msg.getBytes();
-        codingByteArray(code);
-        code=DECODER.decode(code);
-        return new String(code);
-    }
-private void codingByteArray(byte[] code){
-    for (int i = 0; i < code.length; i++) {
-        code[i] ^= CODE_NUMBER;
-    }
-}
     public synchronized void close() {
         try {
             in.close();
