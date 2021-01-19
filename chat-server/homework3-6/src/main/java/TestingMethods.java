6dbc386a57e71587183f4cba1b8cbeda09db0a20
Dmitriy Fomin <79067773397@yandex.ru>
2021-01-20 00:06:35 +0300

    homework3-6 was done

--- chat-server/homework3-6/src/main/java/TestingMethods.java
+++ chat-server/homework3-6/src/main/java/TestingMethods.java
@@ -1,13 +1,36 @@
 import java.util.Arrays;
 
-public class TestingMethods {
+import org.apache.logging.log4j.*;
 
-    public static int[] getFormattedArray(int[] arr){
-        for (int i = arr.length-1; i >=0 ; i--) {
-            if(arr[i]==4){
-                return Arrays.copyOfRange(arr,++i,arr.length);
+public class TestingMethods {
+    private static final Logger LOGGER = LogManager.getLogger(TestingMethods.class);
+
+    public int[] getFormattedArray(int[] arr) {
+        LOGGER.info("start method getFormattedArray");
+        LOGGER.debug("input arr: " + Arrays.toString(arr));
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (arr[i] == 4) {
+                LOGGER.info("Number 4 was found");
+                return Arrays.copyOfRange(arr, ++i, arr.length);
             }
         }
+        LOGGER.warn("There are not number 4");
         throw new RuntimeException("Number 4 was not found into this array");
+    }
+
+    public boolean hasOneAndFour(int[] arr) {
+        LOGGER.info("start method hasOneAndFour");
+        LOGGER.debug("input arr: " + Arrays.toString(arr));
+        boolean hasOne = false, hasFour = false;
+        for (int number : arr) {
+            if (number == 1) hasOne = true;
+            else if (number == 4) hasFour = true;
+            if (hasOne && hasFour) {
+                LOGGER.info("There are numbers 1 and 4");
+                return true;
+            }
+        }
+        LOGGER.warn("There are not 1 or 4");
+        return false;
     }
 }
--- chat-server/homework3-6/src/test/java/TestingMethodsTest.java
+++ chat-server/homework3-6/src/test/java/TestingMethodsTest.java
@@ -1,24 +1,18 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.Before;
 import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.FromDataPoints;
 import org.junit.experimental.theories.Theories;
 import org.junit.experimental.theories.Theory;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.converter.ConvertWith;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.runner.RunWith;
 
-import java.lang.reflect.Executable;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Stream;
 
 @RunWith(Theories.class)
 public class TestingMethodsTest {
+    private static final Logger LOGGER = LogManager.getLogger();
     private TestingMethods testingMethods;
 
     @Before
@@ -26,23 +20,40 @@ public class TestingMethodsTest {
         testingMethods = new TestingMethods();
     }
 
-    @DataPoints
+    @DataPoints("dataForHasOneAndFourTestForFalse")
+    public static int[][] dataForHasOneAndFourTestForFalse = {{1, 1, 1, 1, 1}, {1, 5, 3, 2}, {4, 4, 4, 4, 4}};
+    @DataPoints("dataForHasOneAndFourTestForTrue")
+    public static int[][] dataForHasOneAndFourTestForTrue = {{1, 1, 4, 4, 1}, {4, 3, 2, 6, 1}, {0, -590, 33, 1, 4, 0}};
+    @DataPoints("dataForGetFormattedArrayTestByException")
+    public static int[][] dataForGetFormattedArrayTestByException = {{1, 345, -87, 2, 0}, {}};
+    @DataPoints("dataForGetFormattedArray")
     public static int[][][] dataForGetFormattedArray = {
             {{1, 4, 3, 4, 5}, {5}},
             {{4, 1, 2, 3}, {1, 2, 3}},
             {{33, 645, 4}, {}}};
 
     @Theory
-    public void getFormattedArray(int[][] data) {
+    public void getFormattedArrayTest(@FromDataPoints("dataForGetFormattedArray") int[][] data) {
         Assertions.assertEquals(Arrays.toString(data[1]), Arrays.toString(testingMethods.getFormattedArray(data[0])));
     }
-    @Test
-    public void getFormattedArrayTestByException(){
-       try {
-           testingMethods.getFormattedArray(new int[]{1,77,-100});
-       }catch (RuntimeException e){
-           Assertions.assertEquals("Number 4 was not found into this array", e.getMessage());
-       }
+
+    @Theory
+    public void getFormattedArrayTestByException(@FromDataPoints("dataForGetFormattedArrayTestByException") int[] data) {
+        try {
+            testingMethods.getFormattedArray(data);
+            Assertions.fail();
+        } catch (RuntimeException e) {
+            Assertions.assertEquals("Number 4 was not found into this array", e.getMessage());
+        }
     }
 
+    @Theory
+    public void hasOneAndFourTestForTrue(@FromDataPoints("dataForHasOneAndFourTestForTrue") int[] data) {
+        Assertions.assertTrue(testingMethods.hasOneAndFour(data));
+    }
+
+    @Theory
+    public void hasOneAndFourTestForFalse(@FromDataPoints("dataForHasOneAndFourTestForFalse") int[] data) {
+        Assertions.assertFalse(testingMethods.hasOneAndFour(data));
+    }
 }
